265638f8be91bec858166b0ea58dfd047523b678
vaibhavzalavadiya <vaibhavzalavadiya@ganil.com>
2025-09-02 21:30:27 +0530

    add new files

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput --settings=config.settings_prod
-python manage.py migrate --settings=config.settings_prod
+python manage.py collectstatic --noinput
+python manage.py migrate
